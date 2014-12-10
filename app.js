70f499a45df5bfb415416604512aa819bf47398e
Alex Weidner <side.weidner@gmail.com>
2014-12-10 15:08:04 -0500

    tweak 1

--- app.js
+++ app.js
@@ -96,13 +96,13 @@ function parse_business() {
       })
     })
   })
-  // store_bizs(bizs)
-  console.log(dealos)
+  store_bizs(bizs)
   store_deals(dealos)
 }
 
 // store in couchdb
 function store_bizs(bizs) {
+  console.log(bizs)
   var db_bizs = nano.db.use('bizs')
   db_bizs.bulk({"docs": bizs}, function(err, body) {
     if (err) console.log(err)
